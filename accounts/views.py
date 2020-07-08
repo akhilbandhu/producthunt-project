a5b32a5c553b27e263d43622056ba64df1b89f7e
Akhil <akhilbandhu@gmail.com>
2020-07-08 13:22:37 -0700

    Signup page refined

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
 
 def signup(request):
-    return render(request, 'accounts/signup.html')
+    if request.method == 'POST':
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.get(username=request.POST['username'])
+                return render(request, 'accounts/signup.html', {'error': 'Username already exists'})
+            except User.DoesNotExist:
+                user = User.objects.create_user(request.POST['username'], password='password1')
+                auth.login(request, user)
+                return redirect('homepage')
+        else:
+            return render(request, 'accounts/signup.html', {'error': 'Passwords do not match!'})
+    else:
+        return render(request, 'accounts/signup.html')
 
 def login(request):
     return render(request, 'accounts/login.html')
